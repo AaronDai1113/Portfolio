93e9e1f9465afebdd3bbe0fb848445d3072955f6
AaronDai1113 <aaron11388@outlook.com>
2023-01-08 06:43:33 -0800

    for  rest of it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import Navbar from './scenes/Navbar'
+import Navbar from "./scenes/Navbar";
 import Landing from "./scenes/Landing";
-import DotGroup from './scenes/DotGroup'
-import { useEffect, useState } from "react";
-import LineGradient from './components/LineGradient'
+import DotGroup from "./scenes/DotGroup";
 import MySkills from "./scenes/MySkills";
+import LineGradient from "./components/LineGradient";
 import Projects from "./scenes/Projects";
-import useMediaQuery from './hooks/useMediaQuery';
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
+import useMediaQuery from "./hooks/useMediaQuery";
+import { useEffect, useState } from "react";
+import Testimonials from "./scenes/Testimonials";
 import { motion } from "framer-motion";
 
 function App() {
-  const [selectedPage,setSelectedPage]= useState('home');
+  const [selectedPage, setSelectedPage] = useState("home");
   const [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
@@ -17,6 +20,7 @@ function App() {
     const handleScroll = () => {
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
+        setSelectedPage("home");
       }
       if (window.scrollY !== 0) setIsTopOfPage(false);
     };
@@ -67,6 +71,26 @@ function App() {
         </motion.div>
       </div>
       <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("testimonials")}
+        >
+          <Testimonials />
+        </motion.div>
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("contact")}
+        >
+          <Contact />
+        </motion.div>
+      </div>
+      <Footer />
     </div>
   );
 }
